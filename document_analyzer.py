45c9d1c2b10dbff85afa3676b659eba57b56d47e
nick55808 <72250884+nick55808@users.noreply.github.com>
2022-03-10 21:10:07 -0800

    Add files via upload

--- document_analyzer.py
+++ document_analyzer.py
@@ -2,10 +2,26 @@
 #file1 = open(document.txt) #allows text to be read
 #info = file1.read() #reads individual words
 
-filename = "document.txt"
+#filename = "document.txt"
 
-for line in open(filename, 'r'):
+#for line in open(filename, 'r'):
 	#print(len(line))
-	for i in line:
-		print(i)
+#	for i in line:
+#		print(i)
+dict = {}
+
+with open("document.txt", 'r') as file:
+	for line in file:
+		for word in line.split():
+			if word in dict:
+				dict[word] += 1
+			else:
+				dict[word] = 1
+	lst = sorted(dict, key=dict.get, reverse=True)[:5]
+	#lst = sorted(dict.iteritems(), key=operator.itemgetter(1), reverse=True)[:5]
+	for i in lst:
+		print(f'{i}: {dict[i]}')
+	#print(lst)
+
+
 
