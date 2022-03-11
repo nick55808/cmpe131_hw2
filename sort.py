94d92a92cc8d186592458d118a99323b9fb528a4
nick55808 <72250884+nick55808@users.noreply.github.com>
2022-03-10 23:44:10 -0800

    Add files via upload

--- sort.py
+++ sort.py
@@ -1,19 +1,19 @@
 def sort_list(lst):
 	#if type(x) == list():
 		
-		n = len(lst)
-		i = 0
-		second_i = 0
-		while i < n - 1:
-	#		print ('i = ', i)
-	#		print ('second_i = ', second_i)
-			second_i = n - i - 1
-			while second_i > i:
+	n = len(lst)
+	i = 0
+	second_i = 0
+	while i < n - 1:
+	#	print ('i = ', i)
+	#	print ('second_i = ', second_i)
+		second_i = n - i - 1
+		while second_i > i:
 
-				if lst[i] > lst[second_i]:
-				lst[i], lst[second_i] = lst[second_i], lst[i]
-				second_i -= 1
-			i += 1
+			if lst[i] > lst[second_i]:
+			lst[i], lst[second_i] = lst[second_i], lst[i]
+			second_i -= 1
+		i += 1
 	return lst 
 
 #print (sort_list([1, 3, 2, 7]))
