dece1142b08b6136b74852c7e6cfc78bee452af9
nick55808 <72250884+nick55808@users.noreply.github.com>
2022-03-10 22:16:34 -0800

    Add files via upload

--- timeit.py
+++ timeit.py
@@ -1,13 +1,16 @@
 import time
 
-
-def calculate_time(t):
+def calculate_time(func):
 	def wrapper():
-		print("Total time", t())
+		start = time.time()
+		func()
+		end = time.time()
+		print("Total time", end-start)
 	return wrapper
 
-@calculate_time
-def t():
-	return time.time()
+#@calculate_time
+#def t():
+#	return time.time()
 
-t()
+#t()
+
